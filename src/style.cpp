5192af1093fc2437f0113cd0fc0f55b0412c697b
Caio Marcelo de Oliveira Filho <caio.oliveira@openbossa.org>
2010-03-19 16:30:09 -0300

    Add Style
    This is the basic class in the component styling system discussed
    during the week.  The idea is that a Style will not "paint stuff"
    (imperative painting) like QStyle, but instead it will give a
    component a subtree of other canvas items.  Our implementation will
    consider that the basic 'styleable' components are QGraphicsObjects,
    but the idea can be applied in a more general way.
    
    Besides the common interface, Style also has a basic implementation
    for the populate() method, that look in a table for 'populators'
    previously registered using component name and style name as key.
    
    The basic platform Styles can benefit from this default implementation
    by just changing the style name, and providing new populators.
    
    The 'populator' mechanism serve as a substitute for the
    'switch-on-type' implementation of populate(). One can easily override
    populate() and use 'switch-on-type' if needed.
    
    We are experimenting with the model idea, so we are explicitly passing
    model to the populate() method for now.
    
    Signed-off-by: Caio Marcelo de Oliveira Filho <caio.oliveira@openbossa.org>

---
+++ src/style.cpp
@@ -0,0 +1,161 @@
+/****************************************************************************
+**
+** Copyright (C) 2008-2010 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Qt Software Information (qt-info@nokia.com)
+**
+** This file is part of the Qt Components project on Trolltech Labs.
+**
+** This file may be used under the terms of the GNU General Public
+** License version 2.0 or 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of
+** this file.  Please review the following information to ensure GNU
+** General Public Licensing requirements will be met:
+** http://www.fsf.org/licensing/licenses/info/GPLv2.html and
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** If you are unsure which license is appropriate for your use, please
+** contact the sales department at qt-sales@nokia.com.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#include "style.h"
+
+#include <QtCore/QObject>
+#include <QtDeclarative/QDeclarativeItem>
+#include <QtDeclarative/private/qdeclarativeitemchangelistener_p.h>
+#include <QtDeclarative/private/qdeclarativeitem_p.h>
+#include <QtGui/QGraphicsObject>
+#include <QtGui/QGraphicsWidget>
+#include <QtGui/private/qgraphicsitem_p.h>
+
+const char *ComponentPopulator::styleName = "default";
+
+
+// ### QString vs. const char *
+typedef QPair<QString, QString> Key;
+typedef QHash<Key, ComponentPopulator *> StylePopulatorData;
+Q_GLOBAL_STATIC(StylePopulatorData, stylePopulatorData);
+
+void _q_registerComponentPopulator(const char *componentType,
+                                   const char *styleName,
+                                   ComponentPopulator *pop)
+{
+    Key key(componentType, styleName);
+    StylePopulatorData *table = stylePopulatorData();
+    table->insert(key, pop);
+
+    // ### Do we need mutex to protect the table?
+    // ### warning when registering multiple times?
+}
+
+
+Style *Style::self = 0;
+
+void Style::populate(QGraphicsObject *component, QObject *model)
+{
+    const char *type = component->metaObject()->className();
+
+    // Try a populator for the current style, if not, fallback to 'default'
+    ComponentPopulator *pop = lookupComponentPopulator(type, m_name);
+    if (!pop)
+        pop = lookupComponentPopulator(type, QLatin1String("default"));
+
+    if (pop) {
+        pop->populate(component, model);
+
+        // Make the new children of 'component' follow its geometry.
+        bindChildrenGeometry(component);
+
+    } else {
+        qWarning("Style::populate: couldn't find populator for component '%s' and style '%s'",
+                 type, qPrintable(m_name));
+        return;
+    }
+
+};
+
+Style::Style(const QString &name, QObject *parent)
+    : QObject(parent), m_name(name)
+{
+    Style::self = this;
+}
+
+ComponentPopulator *Style::lookupComponentPopulator(const char *componentType, const QString &name)
+{
+    Key key(componentType, name);
+    StylePopulatorData *table = stylePopulatorData();
+    return (*table)[key];
+}
+
+// Helper class for propagating the geometry of a qmldeclarativeitem
+// to a graphics widget. When the item changes geometry, the widget
+// will change.
+class GraphicsWidgetToDeclarativeItemBinder : public QObject, public QDeclarativeItemChangeListener
+{
+public:
+
+    GraphicsWidgetToDeclarativeItemBinder(QDeclarativeItem *item, QGraphicsWidget *widget)
+        : QObject(), QDeclarativeItemChangeListener(), m_item(item), m_widget(widget) {
+        QDeclarativeItemPrivate *p =
+            static_cast<QDeclarativeItemPrivate *>(QGraphicsItemPrivate::get(m_item));
+        p->addItemChangeListener(this, QDeclarativeItemPrivate::Geometry);
+    }
+
+    virtual ~GraphicsWidgetToDeclarativeItemBinder() {
+        // ### This we can't access, we might have to do this in a
+        // less private-ish fashion (not depending on those
+        // changelisteners...)
+        // QDeclarativeItemPrivate *p =
+        //     static_cast<QDeclarativeItemPrivate *>(QGraphicsItemPrivate::get(m_item));
+        // p->removeItemChangeListener(this, QDeclarativeItemPrivate::Geometry);
+    }
+
+    virtual void itemGeometryChanged(QDeclarativeItem *, const QRectF &newGeometry, const QRectF &) {
+        m_widget->resize(newGeometry.size());
+    }
+
+    QDeclarativeItem *m_item;
+    QGraphicsWidget *m_widget;
+};
+
+
+void Style::bindChildrenGeometry(QGraphicsObject *component)
+{
+    // This function will bind the geometry of the QGraphicsWidget
+    // children. The case for QDeclarativeItem children is treated in QmlStyle.
+
+
+    // The parent is a QGraphicsWidget
+    if (component->isWidget()) {
+        qWarning("Bind for a QGraphicsWidget parent not implemented yet...");
+        // ### Not there yet
+
+        // ### If the widget already has a layout, nothing is needed
+        // (populate added the layout)... if not add a "stacked
+        // layout" with all the toplevel children inside.
+
+        return;
+    }
+
+    // The parent is a QDeclarativeItem, we then use declarative anchors
+    QDeclarativeItem *item = qobject_cast<QDeclarativeItem *>(component);
+    if (item) {
+        // Note that QGraphicsWidget doesn't imply QObject parenthood, thats why we use childItems.
+        QList<QGraphicsItem *> children = item->childItems();
+        for (int i = 0; i < children.count(); i++) {
+            if (!children[i]->isWidget())
+                continue;
+
+            QGraphicsWidget *child = static_cast<QGraphicsWidget *>(children[i]);
+            GraphicsWidgetToDeclarativeItemBinder *binder =
+                new GraphicsWidgetToDeclarativeItemBinder(item, child);
+            binder->setParent(child);
+        }
+    }
+
+    // Only parents that are QGW and QDI are supported right now.
+}
+
