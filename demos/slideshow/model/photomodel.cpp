1f43fb4e26499cc7b6c77df82e221338ed8ebc2b
Anselmo Lacerda S. de Melo <anselmo.melo@openbossa.org>
2010-03-22 02:08:03 -0300

    SlideShow demo: Missing header added
    Added missing header to photomodel.cpp.
    
    Signed-off-by: Anselmo Lacerda S. de Melo <anselmo.melo@openbossa.org>

--- demos/slideshow/model/photomodel.cpp
+++ demos/slideshow/model/photomodel.cpp
@@ -1,4 +1,26 @@
-
+/****************************************************************************
+**
+** Copyright (C) 2008-2010 Nokia Corporation and/or its subsidiary(-ies).
+** Contact: Qt Software Information (qt-info@nokia.com)
+**
+** This file is part of the demonstration applications of the Qt Components project
+** on Trolltech Labs.
+**
+** This file may be used under the terms of the GNU General Public
+** License version 2.0 or 3.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of
+** this file.  Please review the following information to ensure GNU
+** General Public Licensing requirements will be met:
+** http://www.fsf.org/licensing/licenses/info/GPLv2.html and
+** http://www.gnu.org/copyleft/gpl.html.
+**
+** If you are unsure which license is appropriate for your use, please
+** contact the sales department at qt-sales@nokia.com.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
 
 #include <QDeclarativeExtensionPlugin>
 
@@ -7,10 +29,8 @@
 #include <QDeclarativeItem>
 #include <QDeclarativeImageProvider>
 #include <QPixmap>
-#include <QPainter>
 #include <QFileInfo>
 #include <QDir>
-#include <QDebug>
 
 class PhotoProvider : public QDeclarativeImageProvider
 {
