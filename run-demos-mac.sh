6dd4eddc2b0709c8a2d49e8c447a6be6db6fcc90
Marius Bugge Monsen <mmonsen@trolltech.com>
2010-04-12 14:10:58 +0200

    Update the plugin path in the mac version of the demo script.

--- run-demos-mac.sh
+++ run-demos-mac.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 DYLD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
-$QT_BUILD/bin/qml.app/Contents/MacOS/qml -L plugin -L demos/slideshow/model demos/slideshow/$1/main.qml
+$QT_BUILD/bin/qml.app/Contents/MacOS/qml -L modules/components -L demos/slideshow/model demos/slideshow/$1/main.qml
