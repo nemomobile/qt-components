b2cd104032987f04bd567fb2d17c1ef9db65f48e
Marius Bugge Monsen <mmonsen@trolltech.com>
2010-04-14 13:06:42 +0200

    Add mac osx support in the run-mx.sh script.

--- run-mx.sh
+++ run-mx.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
-LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
-qml -L modules/components -L modules/mx $@
+if [ $(uname) == "Darwin" ]; then
+  DYLD_LIBRARY_PATH=lib:$DYLD_LIBRARY_PATH
+  qml -L modules/components -L modules/mx $@
+else
+  LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
+  qml -L modules/components -L modules/mx $@
+fi
