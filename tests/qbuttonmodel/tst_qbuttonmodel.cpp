aa5793419762dab95b7e287d75d8cb7f05cefd5c
Marius Bugge Monsen <mmonsen@trolltech.com>
2010-04-14 17:20:01 +0200

    Add get/set tests to the QButtonModel auto-test.

--- tests/qbuttonmodel/tst_qbuttonmodel.cpp
+++ tests/qbuttonmodel/tst_qbuttonmodel.cpp
@@ -73,7 +73,78 @@ void tst_QButtonModel::cleanup()
 
 void tst_QButtonModel::getSetCheck()
 {
+    // check initial state
+    QCOMPARE(model->isCheckable(), false);
+    QCOMPARE(model->isChecked(), false);
+    QCOMPARE(model->autoRepeat(), false);
+    QCOMPARE(model->autoExclusive(), false);
+    QCOMPARE(model->autoRepeatDelay(), 300);
+    QCOMPARE(model->autoRepeatInterval(), 100);
+    QCOMPARE(model->buttonDown(), false);
+    QCOMPARE(model->mouseOver(), false);
+    QCOMPARE(model->mousePressed(), false);
 
+    // checkable
+    model->setCheckable(true);
+    QCOMPARE(model->isCheckable(), true);
+    model->setCheckable(false);
+    QCOMPARE(model->isCheckable(), false);
+
+    // checked
+    model->setChecked(true);
+    QCOMPARE(model->isChecked(), false);
+
+    model->setCheckable(true);
+    QCOMPARE(model->isChecked(), false);
+
+    model->setChecked(true);
+    QCOMPARE(model->isChecked(), true);
+    model->setChecked(false);
+    QCOMPARE(model->isChecked(), false);
+
+    model->setCheckable(false);
+
+    // autoRepeat
+    model->setAutoRepeat(true);
+    QCOMPARE(model->autoRepeat(), true);
+    model->setAutoRepeat(false);
+    QCOMPARE(model->autoRepeat(), false);
+
+    // autoExclusive
+    model->setAutoExclusive(true);
+    QCOMPARE(model->autoExclusive(), true);
+    model->setAutoExclusive(false);
+    QCOMPARE(model->autoExclusive(), false);
+
+    // autoRepeatDelay
+    model->setAutoRepeatDelay(200);
+    QCOMPARE(model->autoRepeatDelay(), 200);
+    model->setAutoRepeatDelay(300);
+    QCOMPARE(model->autoRepeatDelay(), 300);
+
+    // autoRepeatInterval
+    model->setAutoRepeatInterval(200);
+    QCOMPARE(model->autoRepeatInterval(), 200);
+    model->setAutoRepeatInterval(100);
+    QCOMPARE(model->autoRepeatInterval(), 100);
+
+    // buttonDown
+    model->setButtonDown(true);
+    QCOMPARE(model->buttonDown(), true);
+    model->setButtonDown(false);
+    QCOMPARE(model->buttonDown(), false);
+
+    // mouseOver
+    model->setMouseOver(true);
+    QCOMPARE(model->mouseOver(), true);
+    model->setMouseOver(false);
+    QCOMPARE(model->mouseOver(), false);
+
+    // mousePressed
+    model->setMousePressed(true);
+    QCOMPARE(model->mousePressed(), true);
+    model->setMousePressed(false);
+    QCOMPARE(model->mousePressed(), false);
 }
 
 QTEST_MAIN(tst_QButtonModel)
