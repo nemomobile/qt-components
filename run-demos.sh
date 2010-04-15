7a03ba8d64fba100fd5d71759903eb769a205d1e
Marius Bugge Monsen <mmonsen@trolltech.com>
2010-04-15 12:50:48 +0200

    Remove run-demos-mac.sh and add mac support to run-demos.sh.

--- run-demos.sh
+++ run-demos.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
-qml -L modules/components -L demos/slideshow/model demos/slideshow/$1/main.qml
+if [ $(uname) == "Darwin" ]; then
+  DYLD_LIBRARY_PATH=lib:$DYLD_LIBRARY_PATH
+else
+  LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
+fi
+qml -I modules/components -I demos/slideshow/model demos/slideshow/$1/main.qml
