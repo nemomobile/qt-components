2dfd54a55325686e7e287e86f07c977a1af54d5b
Leonardo Sobral Cunha <leo.cunha@nokia.com>
2010-04-30 16:45:07 +1000

    Update import paths in run-mx, -I instead of -L

--- run-mx.sh
+++ run-mx.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ $(uname) == "Darwin" ]; then
   DYLD_LIBRARY_PATH=lib:$DYLD_LIBRARY_PATH
-  qml -L modules/components -L modules/mx $@
+  qml -I modules/components -I modules/mx $@
 else
   LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
-  qml -L modules/components -L modules/mx $@
+  qml -I modules/components -I modules/mx $@
 fi
