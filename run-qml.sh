173e6ea61f819d5ee10ab30869862193c883f3a2
Marius Bugge Monsen <mmonsen@trolltech.com>
2010-06-17 18:01:44 +0200

    Update the run-qml.sh script to check for 'Darwin'.

--- run-qml.sh
+++ run-qml.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
+if [ $(uname) == "Darwin" ]; then
+DYLD_LIBRARY_PATH=lib:$DYLD_LIBRARY_PATH
+else
 LD_LIBRARY_PATH=lib:$LD_LIBRARY_PATH
-qmlviewer -I modules/components $@
+fi
+qmlviewer -I plugin/QtComponents $@
