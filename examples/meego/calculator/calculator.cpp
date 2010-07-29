431ad7ba57b72b4d3eeff655b80594e49c0a96d8
Lars Knoll <lars.knoll@nokia.com>
2010-07-29 22:39:24 +0200

    make it fast
    Use the raster graphics system and windows style to
    make it go fast.

--- examples/meego/calculator/calculator.cpp
+++ examples/meego/calculator/calculator.cpp
@@ -30,6 +30,8 @@
 
 int main(int argc, char **argv)
 {
+    QApplication::setGraphicsSystem("raster");
+    QApplication::setStyle("windows");
     QApplication app(argc, argv);
     QDir::setCurrent(app.applicationDirPath());
 
