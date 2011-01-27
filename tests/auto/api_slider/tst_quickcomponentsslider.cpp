5826ff35f6efde7cce56eb9a3ce89ce754d5e545
Artur Duque de Souza <artur.souza@openbossa.org>
2011-01-27 14:29:17 -0300

    Fix slider auto test
    This commit changes from an API test to functionality test. API tests
    will be done by the apicheck tool. There are 3 expected failures in
    this test that are related to the way we use enums in QML (ints).
    
    We can't limit the values to known enum values as we map them to ints
    and because of that the test will fail. A new bug is open for this
    task:
    
    http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-414
    
    Signed-off-by: Artur Duque de Souza <artur.souza@openbossa.org>
    Reviewed-by: Thiago de Barros Lacerda <thiago.lacerda@openbossa.org>

--- tests/auto/api_slider/tst_quickcomponentsslider.cpp
+++ tests/auto/api_slider/tst_quickcomponentsslider.cpp
@@ -44,8 +44,8 @@ class tst_quickcomponentsslider : public QObject
     Q_OBJECT
 private slots:
     void initTestCase();
-    void stepSize();
     void value();
+    void stepSize();
     void minimumValue();
     void maximumValue();
     void orientation();
@@ -53,9 +53,6 @@ private slots:
     void pressed();
 
 private:
-    QStringList standard;
-    QString qmlSource;
-
     QObject *componentObject;
 };
 
@@ -67,84 +64,116 @@ void tst_quickcomponentsslider::initTestCase()
     QVERIFY2(componentObject, qPrintable(errors));
 }
 
-void tst_quickcomponentsslider::stepSize()
-{
-    // check presence of property and set initial value
-    QVERIFY( componentObject->setProperty( "stepSize", 2.0 ) );
-    QCOMPARE( componentObject->property("stepSize").toReal(), 2.0 );
-}
-
 void tst_quickcomponentsslider::value()
 {
-    // check presence of property and set initial value
-    QVERIFY( componentObject->setProperty( "minimumValue", 0.0 ) );
-    QVERIFY( componentObject->setProperty( "maximumValue", 100.0 ) );
-    QVERIFY( componentObject->setProperty( "value", 10.0 ) );
-    QCOMPARE( componentObject->property("value").toReal(), 10.0 );
+    // minimum setup to set a value
+    componentObject->setProperty("minimumValue", 0.0);
+    componentObject->setProperty("maximumValue", 100.0);
+
+    // test if setting/getting value works properly
+    QVERIFY(componentObject->setProperty("value", 10.0));
+    QCOMPARE(componentObject->property("value").toReal(), 10.0);
+}
+
+void tst_quickcomponentsslider::stepSize()
+{
+    // test if setting/getting stepSize works properly
+    QVERIFY(componentObject->setProperty("stepSize", 10.0));
+    QCOMPARE(componentObject->property("stepSize").toReal(), 10.0);
+
+    // test if stepSize if working properly (sometimes it may not use
+    // the range model and that's why it's a valid test)
+    componentObject->setProperty("value", 19.0);
+    QCOMPARE(componentObject->property("value").toReal(), 20.0);
 }
 
 void tst_quickcomponentsslider::minimumValue()
 {
-    // check presence of property and set initial value
-    QVERIFY( componentObject->setProperty( "minimumValue", 0.0 ) );
-    QCOMPARE( componentObject->property("minimumValue").toReal(), 0.0 );
+    // test if setting/getting minimumValue works properly
+    QVERIFY(componentObject->setProperty("minimumValue", 0.0));
+    QCOMPARE(componentObject->property("minimumValue").toReal(), 0.0);
 
-    // try to set value below minimum, minimum is already 0
-    componentObject->setProperty( "maximumValue", 100.0 );
-    componentObject->setProperty( "value", 50.0 );
-    componentObject->setProperty( "value", -1.0 );
-    // the minimum limit should prevent change in property
-    QEXPECT_FAIL("", "Not yet blocked by min and max ranges, http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-287", Continue);
-    QVERIFY( componentObject->property("value") == 50.0 );
+    // small setup to test the set of values below minimum
+    componentObject->setProperty("maximumValue", 100.0);
+    componentObject->setProperty("value", 50.0);
+
+    // try to set value below minimum (0)
+    componentObject->setProperty( "value", -15.0 );
+
+    // the value should be equal to the minimum
+    QVERIFY(componentObject->property("value").toReal() ==
+            componentObject->property("minimumValue").toReal());
 }
 
 void tst_quickcomponentsslider::maximumValue()
 {
-    // check presence of property and set initial value
-    QVERIFY( componentObject->setProperty( "maximumValue", 100.0 ) );
-    QCOMPARE( componentObject->property("maximumValue").toReal(), 100.0 );
+    // test if setting/getting maximumValue works properly
+    QVERIFY(componentObject->setProperty("maximumValue", 200.0));
+    QCOMPARE(componentObject->property("maximumValue").toReal(), 200.0);
 
-    // tro ty set value above maximum, maximum is already 100
-    componentObject->setProperty( "minimumValue", 0.0 );
-    componentObject->setProperty( "value", 50.0 );
-    componentObject->setProperty( "value", 101.0 );
-    // the maximum limit should prevent change in property
-    QEXPECT_FAIL("", "Not yet blocked by min and max ranges, http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-287", Continue);
-    QVERIFY( componentObject->property("value") == 50.0 );
+    // small setup to test the set of values above maximum
+    componentObject->setProperty("minimumValue", 0.0);
+    componentObject->setProperty("value", 50.0 );
+
+    // try to set a value above the maximum
+    componentObject->setProperty("value", 210.0 );
+
+    // the value should be equal to the maximum
+    QVERIFY(componentObject->property("value").toReal() ==
+            componentObject->property("maximumValue").toReal());
 }
 
 void tst_quickcomponentsslider::orientation()
 {
-    // check presence of property and set initial value
-    QVERIFY( componentObject->setProperty( "orientation", Qt::Vertical ) );
-    QCOMPARE( componentObject->property("orientation").toInt(), (int)Qt::Vertical );
-    QVERIFY( componentObject->setProperty( "orientation", Qt::Horizontal ) );
-    QCOMPARE( componentObject->property("orientation").toInt(), (int)Qt::Horizontal );
+    // test if setting/getting the orientation works properly
+    QVERIFY(componentObject->setProperty("orientation", Qt::Vertical));
+    QCOMPARE(componentObject->property("orientation").toInt(),
+             (int)Qt::Vertical);
 
-    // make sure no other values than Qt::Orientation are settable
-    componentObject->setProperty( "orientation", 3 );
-    QEXPECT_FAIL("", "Not yet blocked by enum range, http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-287", Continue);
-    QVERIFY( componentObject->property("orientation").toInt() <= (int)Qt::Horizontal
-            && componentObject->property("orientation").toInt() >= (int)Qt::Vertical  );
-    componentObject->setProperty( "orientation", 0 );
-    QEXPECT_FAIL("", "Not yet blocked by enum range, http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-287", Continue);
-    QVERIFY( componentObject->property("orientation").toInt() <= (int)Qt::Horizontal
-            && componentObject->property("orientation").toInt() >= (int)Qt::Vertical  );
-    componentObject->setProperty( "orientation", rand() );
-    QEXPECT_FAIL("", "Not yet blocked by enum range, http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-287", Continue);
-    QVERIFY( componentObject->property("orientation").toInt() <= (int)Qt::Horizontal
-            && componentObject->property("orientation").toInt() >= (int)Qt::Vertical  );
+    QVERIFY(componentObject->setProperty("orientation", Qt::Horizontal));
+    QCOMPARE(componentObject->property("orientation").toInt(),
+             (int)Qt::Horizontal);
 
+    // try to set a higher value
+    componentObject->setProperty("orientation", 3);
+    QEXPECT_FAIL("", "Not yet blocked by enum range,"
+                 "http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-414",
+                 Continue);
+    QVERIFY(componentObject->property("orientation").toInt()
+            <= (int)Qt::Vertical);
+
+    // try to set a lower value
+    componentObject->setProperty("orientation", 0);
+    QEXPECT_FAIL("", "Not yet blocked by enum range,"
+                 "http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-414",
+                 Continue);
+    QVERIFY(componentObject->property("orientation").toInt()
+            >= (int)Qt::Horizontal);
+
+    // try to set random value
+    componentObject->setProperty("orientation", rand());
+    QEXPECT_FAIL("", "Not yet blocked by enum range,"
+                 "http://bugreports.qt.nokia.com/browse/QTCOMPONENTS-414",
+                 Continue);
+    QVERIFY(componentObject->property("orientation").toInt()
+            <= (int)Qt::Horizontal
+            && componentObject->property("orientation").toInt()
+            >= (int)Qt::Vertical);
 }
 
 void tst_quickcomponentsslider::pressed()
 {
+    // as the implementation can change, we can't test the
+    // emission of this signal without relying in specific
+    // internal members of the slider
 }
 
 void tst_quickcomponentsslider::updateValueWhileDragging()
 {
-    QVERIFY( componentObject->setProperty( "updateValueWhileDragging", true ) );
-    QCOMPARE( componentObject->property("updateValueWhileDragging").toBool(), true );
+    // test if setting/getting stepSize works properly
+    QVERIFY(componentObject->setProperty("updateValueWhileDragging", true));
+    QCOMPARE(componentObject->property("updateValueWhileDragging").toBool(),
+             true);
 }
 
 QTEST_MAIN(tst_quickcomponentsslider)
--- tests/auto/api_slider/tst_quickcomponentsslider.qml
+++ tests/auto/api_slider/tst_quickcomponentsslider.qml
@@ -2,14 +2,6 @@ import Qt 4.7
 import Qt.labs.components.native 1.0
 
 Slider {
-   id: testTarget
-   width: 640; height: 480
-
-   minimumValue: 0
-   maximumValue: 100
-   value: 50
-// orientation: Qt.Vertical
-// updateWhileDragging: true
-// stepSize: 2.0
-// pressed is a 'read-only' value
+    id: testTarget
+    width: 640; height: 480
 }
