6d69ceec03416a34e85ce66e1637e4b8a7d1510f
Teppo Eeva <ext-teppo.k.eeva@nokia.com>
2011-03-10 07:45:11 +0200

    Fix for SVG scalability issue related to Symbian image provider
    Merge-request: 365
    Reviewed-by: J-P Nurmi <j-p.nurmi@nokia.com>
    Reviewed-by: Mikko Tuokko <mikko.tuokko@nokia.com>

--- src/symbian/sdeclarativeimageprovider.cpp
+++ src/symbian/sdeclarativeimageprovider.cpp
@@ -39,8 +39,7 @@ SDeclarativeImageProvider::~SDeclarativeImageProvider()
 
 QPixmap SDeclarativeImageProvider::requestPixmap(const QString &id, QSize *size, const QSize &requestedSize)
 {
-    QPixmap pixmap = SIconPool::get(SDeclarative::resolveIconFileName(id),
-                                    requestedSize.isValid() ? requestedSize : QSize(200, 200), Qt::KeepAspectRatio);
+    QPixmap pixmap = SIconPool::get(SDeclarative::resolveIconFileName(id), requestedSize, Qt::KeepAspectRatio);
 
     if (!pixmap.isNull() && size)
         *size = pixmap.size();
