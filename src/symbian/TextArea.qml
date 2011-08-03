nemomobile/qt-components

commit 43c71292507e815092498ada9c90b08c534b1020

Symbian: Fixes to TextTouchController geometry
Merge-request: 909
Reviewed-by: Jaakko Korpela <jaakko.korpela@nokia.com>
Reviewed-by: Samuel Nevala <samuel.nevala@digia.com>

src/symbian/TextArea.qml
@@ -343,14 +343,12 @@ FocusScopeItem {
                 TextTouchController {
                     id: touchController
 
-                    anchors {
-                        top: editor.top; topMargin: -container.verticalMargins
-                        left: editor.left; leftMargin: -container.horizontalMargins
-                    }
-                    height: Math.max(root.height, flick.contentHeight + container.verticalMargins * 2)
-                    width: Math.max(root.width, flick.contentWidth + container.horizontalMargins * 2)
-                    editorScrolledX: flick.contentX - container.horizontalMargins
-                    editorScrolledY: flick.contentY - container.verticalMargins
+                    x: container.x
+                    y: container.y
+                    height: Math.max(container.height, flick.contentHeight + flick.tiny)
+                    width: Math.max(container.width, flick.contentWidth + flick.tiny * 2)
+                    editorScrolledX: flick.contentX
+                    editorScrolledY: flick.contentY
                     copyEnabled: textEdit.selectedText
                     cutEnabled: !textEdit.readOnly && textEdit.selectedText
                     platformInverted: root.platformInverted

commit ebeb272b5728e549d9f15823c41026970f4be6fd

Symbian: Additional editor touch area geometry adjustments
Fixing errors caused by commit
43c71292507e815092498ada9c90b08c534b1020. Especially selection
handles were not correctly taken into account.

Merge-request: 912
Reviewed-by: Samuel Nevala <samuel.nevala@digia.com>

src/symbian/TextArea.qml
@@ -343,12 +343,15 @@ FocusScopeItem {
                 TextTouchController {
                     id: touchController
 
-                    x: container.x
-                    y: container.y
-                    height: Math.max(container.height, flick.contentHeight + flick.tiny)
-                    width: Math.max(container.width, flick.contentWidth + flick.tiny * 2)
-                    editorScrolledX: flick.contentX
-                    editorScrolledY: flick.contentY
+                    //  selection handles require touch area geometry to differ from TextEdit's geometry
+                    anchors {
+                        top: editor.top; topMargin: -container.verticalMargins / 2
+                        left: editor.left; leftMargin: -container.horizontalMargins / 2
+                    }
+                    height: Math.max(root.height, flick.contentHeight + container.verticalMargins * 2)
+                    width: Math.max(root.width, flick.contentWidth + container.horizontalMargins * 2)
+                    editorScrolledX: flick.contentX - (container.horizontalMargins / 2)
+                    editorScrolledY: flick.contentY - (container.verticalMargins / 2)
                     copyEnabled: textEdit.selectedText
                     cutEnabled: !textEdit.readOnly && textEdit.selectedText
                     platformInverted: root.platformInverted

src/symbian/TextField.qml
@@ -228,6 +228,7 @@ FocusScope {
                 TextTouchController {
                     id: touchController
 
+                    //  selection handles require touch area geometry to differ from TextInput's geometry
                     anchors {
                         left: parent.left;
                         leftMargin: -flick.tiny
@@ -235,8 +236,8 @@ FocusScope {
                     }
                     height: root.height
                     width: Math.max(root.width, flick.contentWidth + flick.tiny * 2)
-                    editorScrolledX: flick.contentX
-                    editorScrolledY: flick.contentY
+                    editorScrolledX: flick.contentX - container.anchors.leftMargin - flick.tiny
+                    editorScrolledY: 0
                     copyEnabled: textInput.selectedText
                     cutEnabled: !textInput.readOnly && textInput.selectedText
                     platformInverted: root.platformInverted